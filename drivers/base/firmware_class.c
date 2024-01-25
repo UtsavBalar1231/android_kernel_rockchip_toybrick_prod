1a6d7bee0f500531b78509510a7ca903280b907f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-01-25 10:54:10 +0530

    drivers: firmware_class: Add AOSP vendor and system path for firmware loading
    Change-Id: I2347e939462df81be66f9047f0041cdbb7c0fbc3
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -277,6 +277,8 @@ static void fw_free_buf(struct firmware_buf *buf)
 static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
+	"/vendor/etc/firmware"
+	"/system/etc/firmware"
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
