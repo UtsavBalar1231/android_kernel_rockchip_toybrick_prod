d322360f18aed8a0639a03bdf5f659907b52e5a3
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-02-06 14:12:21 +0530

    make.sh: Update
    Change-Id: I20d323afa1cbbea1ae98c700f3f15ff043374c02
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- make.sh
+++ make.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 VERSION="2.0"
-JOB=`sed -n "N;/processor/p" /proc/cpuinfo|wc -l`
-DTB=amolk
+JOB=$(sed -n "N;/processor/p" /proc/cpuinfo | wc -l)
+DTB=rk3399pro-amolk
 
-function help()
-{
+function help() {
 	echo "Usage: ./make.sh os board"
 	echo
 	echo "Parameter:"
@@ -23,78 +22,75 @@ function help()
 	echo "     ./make.sh linux prod"
 	echo "     ./make.sh linux prop"
 	echo "     ./make.sh linux 96ai"
-    echo "     ./make.sh linux prox"
+	echo "     ./make.sh linux prox"
 }
 
-function make_extlinux_conf()
-{
-	echo "label rockchip-kernel-4.4" > boot_linux/extlinux/extlinux.conf
-	echo "	kernel /extlinux/Image" >> boot_linux/extlinux/extlinux.conf
-	echo "	fdt /extlinux/toybrick.dtb" >> boot_linux/extlinux/extlinux.conf
-	echo "	append  earlycon=uart8250,mmio32,0xff1a0000 initrd=/initramfs-toybrick-${VERSION}.img root=PARTUUID=614e0000-0000-4b53-8000-1d28000054a9 rw rootwait rootfstype=ext4" >> boot_linux/extlinux/extlinux.conf
+function make_extlinux_conf() {
+	echo "label rockchip-kernel-4.4" >boot_linux/extlinux/extlinux.conf
+	echo "	kernel /extlinux/Image" >>boot_linux/extlinux/extlinux.conf
+	echo "	fdt /extlinux/toybrick.dtb" >>boot_linux/extlinux/extlinux.conf
+	echo "	append  earlycon=uart8250,mmio32,0xff1a0000 initrd=/initramfs-toybrick-${VERSION}.img root=PARTUUID=614e0000-0000-4b53-8000-1d28000054a9 rw rootwait rootfstype=ext4" >>boot_linux/extlinux/extlinux.conf
 }
 
-
-if [ $# -lt 2 ];then
+if [ $# -lt 2 ]; then
 	help
 	exit 1
 fi
 
 case $1 in
-	android)
-		make rockchip_defconfig
-		make ARCH=arm64 rk3399pro-${DTB}-android.img -j${JOB}
-        ;;
-	linux)
-		rm -rf boot_linux
-		mkdir -p boot_linux/extlinux
+android)
+	make rockchip_defconfig
+	make ARCH=arm64 "$DTB"-android.img -j${JOB}
+	;;
+linux)
+	rm -rf boot_linux
+	mkdir -p boot_linux/extlinux
 
-		make rockchip_linux_defconfig
-		case $2 in
-			prod)
-				DTB=rk3399pro-amolk-linux
-				;;
-			prop)
-				DTB=rk3399pro-toybrick-prop-linux
-				;;
-			96ai)
-				DTB=rk3399pro-toybrick-96ai-linux
-				;;
-			prox)
-				DTB=rk3399pro-toybrick-prox-linux
-				make ARCH=arm64 ${DTB}-edp.img -j${JOB}
-				make ARCH=arm64 ${DTB}-mipi.img -j${JOB}
-				make ARCH=arm64 ${DTB}-u2.img -j${JOB}
-				make ARCH=arm64 ${DTB}-imx258.img -j${JOB}
-				cp -f arch/arm64/boot/dts/rockchip/${DTB}-u2.dtb boot_linux/extlinux/toybrick-u2.dtb
-				cp -f arch/arm64/boot/dts/rockchip/${DTB}-edp.dtb boot_linux/extlinux/toybrick-edp.dtb
-				cp -f arch/arm64/boot/dts/rockchip/${DTB}-mipi.dtb boot_linux/extlinux/toybrick-mipi.dtb
-				cp -f arch/arm64/boot/dts/rockchip/${DTB}-imx258.dtb boot_linux/extlinux/toybrick-imx258.dtb
-                ;;
-			*)
-				help
-				exit 1
-				;;
-		esac
-		make ARCH=arm64 ${DTB}.img -j${JOB}
-
-		cp -f arch/arm64/boot/dts/rockchip/${DTB}.dtb boot_linux/extlinux/toybrick.dtb
-		cp -f arch/arm64/boot/dts/rockchip/${DTB}.dtb boot_linux/extlinux/toybrick-default.dtb
-		cp -f arch/arm64/boot/Image boot_linux/extlinux/
-		make_extlinux_conf
-		cp -f initramfs-toybrick-${VERSION}.img boot_linux/
-		cp -f rescue.sh boot_linux/
-		if [ "`uname -i`" == "aarch64" ]; then
-			echo y | mke2fs -b 4096 -d boot_linux -i 8192 -t ext2 boot_linux.img $((64 * 1024 * 1024 / 4096))
-		else
-			genext2fs -b 32768 -B $((64 * 1024 * 1024 / 32768)) -d boot_linux -i 8192 -U boot_linux.img
-		fi
+	make rockchip_linux_defconfig
+	case $2 in
+	prod)
+		DTB=$DTB-linux
+		;;
+	prop)
+		DTB=rk3399pro-toybrick-prop-linux
+		;;
+	96ai)
+		DTB=rk3399pro-toybrick-96ai-linux
+		;;
+	prox)
+		DTB=rk3399pro-toybrick-prox-linux
+		make ARCH=arm64 "$DTB"-edp.img -j${JOB}
+		make ARCH=arm64 "$DTB"-mipi.img -j${JOB}
+		make ARCH=arm64 "$DTB"-u2.img -j${JOB}
+		make ARCH=arm64 "$DTB"-imx258.img -j${JOB}
+		cp -f arch/arm64/boot/dts/rockchip/"$DTB"-u2.dtb boot_linux/extlinux/toybrick-u2.dtb
+		cp -f arch/arm64/boot/dts/rockchip/"$DTB"-edp.dtb boot_linux/extlinux/toybrick-edp.dtb
+		cp -f arch/arm64/boot/dts/rockchip/"$DTB"-mipi.dtb boot_linux/extlinux/toybrick-mipi.dtb
+		cp -f arch/arm64/boot/dts/rockchip/"$DTB"-imx258.dtb boot_linux/extlinux/toybrick-imx258.dtb
 		;;
 	*)
-		help
-		exit 1
+		DTB=$DTB-linux
 		;;
+	esac
+	echo "Building linux kernel for ${DTB}" >&2
+	make ARCH=arm64 "$DTB".img -j${JOB}
+
+	cp -f arch/arm64/boot/dts/rockchip/"$DTB".dtb boot_linux/extlinux/toybrick.dtb
+	cp -f arch/arm64/boot/dts/rockchip/"$DTB".dtb boot_linux/extlinux/toybrick-default.dtb
+	cp -f arch/arm64/boot/Image boot_linux/extlinux/
+	make_extlinux_conf
+	cp -f initramfs-toybrick-"$VERSION".img boot_linux/
+	cp -f rescue.sh boot_linux/
+	if [ "$(uname -i)" == "aarch64" ]; then
+		echo y | mke2fs -b 4096 -d boot_linux -i 8192 -t ext2 boot_linux.img $((64 * 1024 * 1024 / 4096))
+	else
+		genext2fs -b 32768 -B $((64 * 1024 * 1024 / 32768)) -d boot_linux -i 8192 -U boot_linux.img
+	fi
+	;;
+*)
+	help
+	exit 1
+	;;
 esac
 
 exit 0
-
